0a5d82eb2e1f7b8af1dfe10758c01041c3595c0e
Garawanta Hurekity <Astronomyc9@gmail.com>
2023-11-09 18:58:44 +0900

    f task

--- IntroductionToC#/Program.cs
+++ IntroductionToC#/Program.cs
@@ -9,25 +9,31 @@ namespace DataTypes
     class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine($"bool: {sizeof(bool)}, values: {true} or {false}");
-            Console.WriteLine($"char: {sizeof(char)}"); //unicode
-                                                        //short, ushort;
-                                                        //int, uint;
-            Console.WriteLine($"short: {sizeof(short)} Байт памяти, диапазон значений: {short.MinValue} ... {short.MaxValue}");
-            Console.WriteLine($"short: {sizeof(ushort)} Байт памяти, диапазон значений: {ushort.MinValue} ... {ushort.MaxValue}");
+            /*  Console.WriteLine($"bool: {sizeof(bool)}, values: {true} or {false}");
+              Console.WriteLine($"char: {sizeof(char)}"); //unicode
+                                                          //short, ushort;
+                                                          //int, uint;
+              Console.WriteLine($"short: {sizeof(short)} Байт памяти, диапазон значений: {short.MinValue} ... {short.MaxValue}");
+              Console.WriteLine($"short: {sizeof(ushort)} Байт памяти, диапазон значений: {ushort.MinValue} ... {ushort.MaxValue}");
 
-            Console.WriteLine($"float: {sizeof(float)} Байт памяти, диапазон значений: {float.MinValue} ... {float.MaxValue}");
-            Console.WriteLine($"double: {sizeof(double)} Байт памяти, диапазон значений: {double.MinValue} ... {double.MaxValue}");
-            Console.WriteLine($"decimal: {sizeof(decimal)} Байт памяти, диапазон значений: {decimal.MinValue} ... {decimal.MaxValue}");
+              Console.WriteLine($"float: {sizeof(float)} Байт памяти, диапазон значений: {float.MinValue} ... {float.MaxValue}");
+              Console.WriteLine($"double: {sizeof(double)} Байт памяти, диапазон значений: {double.MinValue} ... {double.MaxValue}");
+              Console.WriteLine($"decimal: {sizeof(decimal)} Байт памяти, диапазон значений: {decimal.MinValue} ... {decimal.MaxValue}");
 
-            Console.WriteLine('+'.GetType());
-            Console.WriteLine(5.GetType());
-            Console.WriteLine(5.5.GetType());
-            Console.WriteLine(5.5f.GetType());
-            Console.WriteLine(5.5m.GetType());  //m - Money
-            Console.WriteLine();     
-            // WSID
+              Console.WriteLine('+'.GetType());
+              Console.WriteLine(5.GetType());
+              Console.WriteLine(5.5.GetType());
+              Console.WriteLine(5.5f.GetType());
+              Console.WriteLine(5.5m.GetType());  //m - Money
+            */
+            Console.Write("Введите число для перевода: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"Ваше число: {number}");
+            int f_number = Convert.ToInt32(number);
+            int remainder = Convert.ToInt32((number - f_number)*100);
+            Console.WriteLine($" --> {f_number}р{remainder}коп");  // if .>5 then .x=100-secpart of digit in minus position
         }
     }
 }
