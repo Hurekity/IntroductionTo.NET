b25ff80fa6ca0c94bf11f0c6fb2b1bcfd2e1c5a0
Garawanta Hurekity <Astronomyc9@gmail.com>
2023-11-09 20:43:23 +0900

    calc done(not completely)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,10 +10,17 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Write ur first digit: ");
-            int f_digit = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Write an operator: ");
-            
+            Console.Write("Write an expression: ");
+            string expression = Console.ReadLine();
+            expression = expression.Replace(".", ",");
+            Console.WriteLine(expression);
+            String[] numbers = expression.Split('+', '-', '*', '/');
+            double a = Convert.ToDouble(numbers[0]);
+            double b = Convert.ToDouble(numbers[1]);
+            if(expression.Contains("+")) Console.WriteLine($"{a} + {b} = {a+b}");
+            else if (expression.Contains("-")) Console.WriteLine($"{a} - {b} = {a-b}");
+            else if (expression.Contains("*")) Console.WriteLine($"{a} * {b} = {a*b}");
+            else if (expression.Contains("/")) Console.WriteLine($"{a} / {b} = {a/b}");
         }
     }
 }
