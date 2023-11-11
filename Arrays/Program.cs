c8dc54bb403fdd1f2241214b1fe5d7866d8adb4b
Garawanta Hurekity <Astronomyc9@gmail.com>
2023-11-11 18:57:09 +0900

    uncompleted

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -109,6 +109,7 @@ namespace Arrays
                 }
                 Console.WriteLine();    
             }
+            
         }
     }
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,17 +10,23 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Write an expression: ");
-            string expression = Console.ReadLine();
-            expression = expression.Replace(".", ",");
-            Console.WriteLine(expression);
-            String[] numbers = expression.Split('+', '-', '*', '/');
-            double a = Convert.ToDouble(numbers[0]);
-            double b = Convert.ToDouble(numbers[1]);
-            if(expression.Contains("+")) Console.WriteLine($"{a} + {b} = {a+b}");
-            else if (expression.Contains("-")) Console.WriteLine($"{a} - {b} = {a-b}");
-            else if (expression.Contains("*")) Console.WriteLine($"{a} * {b} = {a*b}");
-            else if (expression.Contains("/")) Console.WriteLine($"{a} / {b} = {a/b}");
+            char repeat = 'Y';
+            while (repeat == 'Y')
+            {
+                Console.Write("Write an expression: ");
+                string expression = Console.ReadLine();
+                expression = expression.Replace(".", ",");
+                Console.WriteLine(expression);
+                String[] numbers = expression.Split('+', '-', '*', '/');
+                double a = Convert.ToDouble(numbers[0]);
+                double b = Convert.ToDouble(numbers[1]);
+                if (expression.Contains("+")) Console.WriteLine($"{a} + {b} = {a + b}");
+                else if (expression.Contains("-")) Console.WriteLine($"{a} - {b} = {a - b}");
+                else if (expression.Contains("*")) Console.WriteLine($"{a} * {b} = {a * b}");
+                else if (expression.Contains("/")) Console.WriteLine($"{a} / {b} = {a / b}");
+                Console.Write("Do u wanna to continue working? (Y/N): ");
+                repeat = Convert.ToChar(Console.ReadLine());
+            }
         }
     }
 }
